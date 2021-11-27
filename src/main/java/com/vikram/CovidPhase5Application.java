2ee4a259333f6d1509eb29f0114734be7f246259
Vikram <vikrammcreddy@gmail.com>
2021-11-28 00:11:23 +0530

    release 1.0

---
+++ src/main/java/com/vikram/CovidPhase5Application.java
@@ -0,0 +1,13 @@
+package com.vikram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidPhase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidPhase5Application.class, args);
+	}
+
+}
